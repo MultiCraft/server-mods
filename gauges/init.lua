b056219a562fe7819e1d71a6002406896b2d36aa
MoNTE48 <MoNTE48@mail.ua>
2021-04-06 15:25:42 +0200

    Gauges: update

--- gauges/init.lua
+++ gauges/init.lua
@@ -10,6 +10,16 @@ local function add_gauge(player)
 	end
 end
 
+local function check_visibility(player)
+	if minetest.global_exists("invisibility") then
+		if invisibility[player:get_player_name()] == true then
+			return true
+		end
+	end
+
+	return false
+end
+
 minetest.register_entity("gauges:hp_bar", {
 	visual = "sprite",
 	visual_size = {x = 0.8, y = 0.8/16, z = 0.8},
@@ -28,13 +38,24 @@ minetest.register_entity("gauges:hp_bar", {
 		elseif vector_distance(player:get_pos(), gauge:get_pos()) > 3 then
 			gauge:remove()
 			add_gauge(player)
+
+			minetest.log("warning", "[GAUGES] Distance between player detected!")
+			return
+		end
+
+		-- Hide gauge if player is invisible
+		if check_visibility(player) then
+			gauge:set_properties({
+				textures = {"blank.png"}
+			})
+			self.hide = true
 			return
 		end
 
 		local hp     = player:get_hp()     <= 20 and player:get_hp()     or 20
 		local breath = player:get_breath() <  10 and player:get_breath() or 11
 
-		if self.hp ~= hp or self.breath ~= breath then
+		if self.hp ~= hp or self.breath ~= breath or self.hide then
 			gauge:set_properties({
 				textures = {
 					"health_" .. hp .. ".png^" ..
@@ -43,6 +64,7 @@ minetest.register_entity("gauges:hp_bar", {
 			})
 			self.hp     = hp
 			self.breath = breath
+			self.hide   = nil
 		end
 	end
 })
@@ -50,5 +72,22 @@ minetest.register_entity("gauges:hp_bar", {
 if enable_damage then
 	minetest.register_on_joinplayer(function(player)
 		minetest.after(1, add_gauge, player)
+	end)
+
+	minetest.register_on_respawnplayer(function(player)
+		minetest.after(1, function()
+			local name = player:get_player_name()
+			local pos = player:get_pos()
+
+			for _, obj in pairs(minetest.get_objects_inside_radius(pos, 1)) do
+				local ent = obj:get_luaentity()
+				if ent and ent.name == "gauges:hp_bar" then
+					return
+				end
+			end
+
+			-- Re-add gauge if removed on death
+			add_gauge(player)
+		end)
 	end)
 end
